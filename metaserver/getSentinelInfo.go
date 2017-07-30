80307c4efbac0bd7e2bce748978c58e3a867f5c7
levy5307 <zlw5307@163.com>
2017-07-30 19:44:32 +0800

    store meta data into redis

--- metaserver/getSentinelInfo.go
+++ metaserver/getSentinelInfo.go
@@ -1,12 +1,13 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"regexp"
+	"strconv"
+	"strings"
 	"sync/atomic"
 	"time"
-    "strconv"
-    "strings"
 )
 
 import (
@@ -23,7 +24,8 @@ var (
 )
 
 const (
-	SentinelAddr = "192.168.160.137:30000"
+	SentinelAddr = "192.168.1.105:40000"
+	DBAddr = "192.168.1.105:20000"
 )
 
 type NetAddress struct {
@@ -38,14 +40,14 @@ type Meta struct {
 	Slaves []NetAddress
 }
 
-func getSlavesInfo(masterAddr NetAddress)([]NetAddress, error) {
-    var (
-        master string
-        slaves []NetAddress
-        slave  NetAddress
-    )
+func getSlavesInfo(masterAddr NetAddress) ([]NetAddress, error) {
+	var (
+		master string
+		slaves []NetAddress
+		slave  NetAddress
+	)
 
-    master = fmt.Sprintf("%s:%s", masterAddr.IP, strconv.Itoa(masterAddr.Port))
+	master = fmt.Sprintf("%s:%s", masterAddr.IP, strconv.Itoa(masterAddr.Port))
 
 	// connect redis
 	conn, err := redis.Dial("tcp", master)
@@ -61,49 +63,49 @@ func getSlavesInfo(masterAddr NetAddress)([]NetAddress, error) {
 	}
 
 	// parse slave ip, using regular expression
-    ipReg := regexp.MustCompile(`ip=(([0-9]{1,}.){3}[0-9])`)
-    ips := ipReg.FindAllString(allStr, -1)
+	ipReg := regexp.MustCompile(`ip=(([0-9]{1,}.){3}[0-9])`)
+	ips := ipReg.FindAllString(allStr, -1)
 
-    // parse slave port
-    portReg := regexp.MustCompile(`port=[0-9]{1,}`)
-    ports := portReg.FindAllString(allStr, -1)
+	// parse slave port
+	portReg := regexp.MustCompile(`port=[0-9]{1,}`)
+	ports := portReg.FindAllString(allStr, -1)
 
-    // if ip count != port count, return error
-    if len(ips) != len(ports) {
+	// if ip count != port count, return error
+	if len(ips) != len(ports) {
 		return slaves, errors.Wrapf(err, fmt.Sprintf("fail to exec command info:sentinel"))
-    }
+	}
 
-    // extract addr/port from names and addrs
-    for i:=0; i<len(ips); i++ {
-       var (
-           tmpStrs []string 
-       )
-       equalReg := regexp.MustCompile(`=`)
+	// extract addr/port from names and addrs
+	for i := 0; i < len(ips); i++ {
+		var (
+			tmpStrs []string
+		)
+		equalReg := regexp.MustCompile(`=`)
 
-       // split ip string and get ip
-       tmpStrs = equalReg.Split(ips[i], -1)// use '=' to split the ip string
-       slave.IP = tmpStrs[1] 
+		// split ip string and get ip
+		tmpStrs = equalReg.Split(ips[i], -1) // use '=' to split the ip string
+		slave.IP = tmpStrs[1]
 
-       // split port string and get port 
-       tmpStrs = equalReg.Split(ports[i], -1)
-       slave.Port, _ = strconv.Atoi(tmpStrs[1])
+		// split port string and get port
+		tmpStrs = equalReg.Split(ports[i], -1)
+		slave.Port, _ = strconv.Atoi(tmpStrs[1])
 
-       // insert slave to slaves 
-       slaves = append(slaves, slave)
-    }
+		// insert slave to slaves
+		slaves = append(slaves, slave)
+	}
 
-    return slaves, nil
+	return slaves, nil
 }
 
 // get sentinel info once
 func getSentinelInfo() ([]Meta, error) {
 	var (
 		err        error
-        metas      []Meta
+		metas      []Meta
 		nameReg    *regexp.Regexp
 		addressReg *regexp.Regexp
-        names      []string
-        addrs      []string
+		names      []string
+		addrs      []string
 	)
 
 	// connect redis
@@ -120,46 +122,46 @@ func getSentinelInfo() ([]Meta, error) {
 	}
 
 	// parse name, using regular expression
-    nameReg = regexp.MustCompile(`(master[0-9]{1,}):name=([^0-9]{1,})[0-9]`)
-    names = nameReg.FindAllString(allStr, -1)
+	nameReg = regexp.MustCompile(`(master[0-9]{1,}):name=([^0-9]{1,})[0-9]`)
+	names = nameReg.FindAllString(allStr, -1)
 
 	// parse address:port, using regular expression
 	addressReg = regexp.MustCompile(`address=(([0-9]{1,}.){3})([0-9]{1,}):([0-9]{1,})`)
-    addrs = addressReg.FindAllString(allStr, -1)
+	addrs = addressReg.FindAllString(allStr, -1)
 
-    // if name count != addr count, return error
-    if len(names) != len(addrs) {
+	// if name count != addr count, return error
+	if len(names) != len(addrs) {
 		return metas, errors.Wrapf(err, fmt.Sprintf("fail to exec command info:sentinel"))
-    }
-    
-    // extract name/addr/port from names and addrs
-    for i:=0; i<len(names); i++ {
-       var (
-           tmpMeta Meta
-           tmpStrs []string 
-       )
-       equalReg := regexp.MustCompile(`=`)
-       colonReg := regexp.MustCompile(`:`)
+	}
 
-       // split name string and get master name
-       tmpStrs = equalReg.Split(names[i], -1)// use ‘=’ to split the name string
-       tmpMeta.Name = tmpStrs[1]
+	// extract name/addr/port from names and addrs
+	for i := 0; i < len(names); i++ {
+		var (
+			tmpMeta Meta
+			tmpStrs []string
+		)
+		equalReg := regexp.MustCompile(`=`)
+		colonReg := regexp.MustCompile(`:`)
 
-       // split address string and get master addr(ip, port)
-       tmpStrs = equalReg.Split(addrs[i], -1)// use '=' to split the address string
-       tmpStrs = colonReg.Split(tmpStrs[1], -1)// use ':' to split the sub string
-       tmpMeta.Master.IP = tmpStrs[0] 
-       tmpMeta.Master.Port, _  = strconv.Atoi(tmpStrs[1])
+		// split name string and get master name
+		tmpStrs = equalReg.Split(names[i], -1) // use ‘=’ to split the name string
+		tmpMeta.Name = tmpStrs[1]
 
-       // get slave info
-       tmpMeta.Slaves, err = getSlavesInfo(tmpMeta.Master)
-       if err != nil {
+		// split address string and get master addr(ip, port)
+		tmpStrs = equalReg.Split(addrs[i], -1)   // use '=' to split the address string
+		tmpStrs = colonReg.Split(tmpStrs[1], -1) // use ':' to split the sub string
+		tmpMeta.Master.IP = tmpStrs[0]
+		tmpMeta.Master.Port, _ = strconv.Atoi(tmpStrs[1])
+
+		// get slave info
+		tmpMeta.Slaves, err = getSlavesInfo(tmpMeta.Master)
+		if err != nil {
 			Log.Warn("failed to get redis slave info, error:%#v", err)
-        }
+		}
 
-       // insert theMeta to metas 
-       metas = append(metas, tmpMeta)
-    }
+		// insert theMeta to metas
+		metas = append(metas, tmpMeta)
+	}
 
 	return metas, nil
 }
@@ -167,10 +169,10 @@ func getSentinelInfo() ([]Meta, error) {
 // get sentinel info periodically
 func GetSentinelInfo(wh *gxtime.Wheel, span time.Duration) {
 	var (
-		err  error
+		err   error
 		metas []Meta
-		str string
-		ver  int32
+		str   string
+		ver   int32
 	)
 
 	for {
@@ -182,53 +184,57 @@ func GetSentinelInfo(wh *gxtime.Wheel, span time.Duration) {
 				break
 			}
 			ver = atomic.AddInt32(&version, 1)
-			infoMap[int(ver)] = metas 
+			infoMap[int(ver)] = metas
+			SetMeta(int(ver), metas)
 			delete(infoMap, int(ver-1024))
+			DelMeta(int(ver-1024))
 
-        case str = <-ch:
-            // get latest metas info
+		case str = <-ch:
+			// get latest metas info because of received +switch-master message
 			ver = atomic.LoadInt32(&version)
 			metas = infoMap[int(ver)]
-            
-            // use ‘ ’ to split the name string
-            // split: 0-master name; 
-            //        1-old master ip; 2-old master port
-            //        3-new master ip; 4-new master port
-            spaceReg := regexp.MustCompile(` `)
-            splitStrs := spaceReg.Split(str, -1)
 
-            // if the count of split string is less than 5, skip
-            if len(splitStrs) < 5 {
-                continue
-            }
+			// use ‘ ’ to split the name string
+			// split: 0-master name;
+			//        1-old master ip; 2-old master port
+			//        3-new master ip; 4-new master port
+			spaceReg := regexp.MustCompile(` `)
+			splitStrs := spaceReg.Split(str, -1)
 
-            // if convert from string to int returns error, skip
-            newMasterPort, err := strconv.Atoi(splitStrs[4])
-            if err != nil {
-                continue
-            }
+			// if the count of split string is less than 5, skip
+			if len(splitStrs) < 5 {
+				continue
+			}
 
-            // find the right master-slave instance`s meta info
-            for i, _ := range metas {
-                if strings.Compare(metas[i].Name, splitStrs[0]) == 0 {
-                    // update master info with the new promoted node info
-                    metas[i].Master.IP = splitStrs[3]
-                    metas[i].Master.Port = newMasterPort
+			// if convert from string to int returns error, skip
+			newMasterPort, err := strconv.Atoi(splitStrs[4])
+			if err != nil {
+				continue
+			}
 
-                    // delete the slave info which promoted to master with old master info
-                    for j, _ := range metas[i].Slaves {
-                        if strings.Compare(metas[i].Slaves[j].IP, splitStrs[3]) == 0 && metas[i].Slaves[j].Port == newMasterPort {
-                           metas[i].Slaves = append(metas[i].Slaves[:j], metas[i].Slaves[j+1:]...)
-                           break;
-                        }
-                    }
-                    break
-                }// end if
-            }// end for
+			// find the right master-slave instance`s meta info
+			for i, _ := range metas {
+				if strings.Compare(metas[i].Name, splitStrs[0]) == 0 {
+					// update master info with the new promoted node info
+					metas[i].Master.IP = splitStrs[3]
+					metas[i].Master.Port = newMasterPort
+
+					// delete the slave info which promoted to master with old master info
+					for j, _ := range metas[i].Slaves {
+						if strings.Compare(metas[i].Slaves[j].IP, splitStrs[3]) == 0 && metas[i].Slaves[j].Port == newMasterPort {
+							metas[i].Slaves = append(metas[i].Slaves[:j], metas[i].Slaves[j+1:]...)
+							break
+						}
+					}
+					break
+				} // end if
+			} // end for
 
 			ver = atomic.AddInt32(&version, 1)
-			infoMap[int(ver)] = metas 
+			infoMap[int(ver)] = metas
+			SetMeta(int(ver), metas)
 			delete(infoMap, int(ver-1024))
+			DelMeta(int(ver-1024))
 		}
 	}
 }
@@ -278,3 +284,49 @@ func main() {
 	// get sentinel info periodically with timewheel
 	GetSentinelInfo(wh, gxtime.TimeMillisecondDuration(3000))
 }
+
+func SetMeta(ver int, data []Meta) error {
+	// connect redis
+	conn, err := redis.Dial("tcp", DBAddr)
+	if err != nil {
+		fmt.Println("connect error: ", err)
+		return err
+	}
+	defer conn.Close()
+
+	// convert data from Meta to byte
+	value, err := json.Marshal(data)
+	if err != nil {
+		Log.Warn("json marshal err,%s", err)
+		return err
+	}
+
+	_, err = conn.Do("SET", ver, value)
+	if err != nil {
+		fmt.Println("store error\n")
+		return err
+	}
+
+	return nil
+}
+
+func DelMeta(ver int) (err error) {
+
+	// connect redis
+	conn, err := redis.Dial("tcp", DBAddr)
+	if err != nil {
+		fmt.Println("connect error: ", err)
+		return err
+	}
+	defer conn.Close()
+
+	// delete data which version is ver
+	_, err = conn.Do("DEL", ver)
+	if err != nil {
+		fmt.Println("delete error\n")
+		return err
+	}
+
+	return nil
+}
+
